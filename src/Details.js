7de0c10261e84dbb30a5ca9b17f502bb7541b2e7
CristoforosChoras <christopher.choras@yahoo.com>
2022-06-15 21:48:40 +0300

    Class Component  carousel

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
   constructor() {
@@ -20,10 +21,11 @@ class Details extends Component {
       return <h2>loading … </h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } = this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />;
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
